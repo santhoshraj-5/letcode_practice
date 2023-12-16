a6fc5211218605fd376892c5b6ad19ed4453a510
Admin <Admin@DESKTOP-1E7MQ28>
2023-12-16 12:26:27 +0530

    dataprovider part 2

--- automation_practice/src/test/java/TestNG_practice/dataprovider_anotherclass_practice.java
+++ automation_practice/src/test/java/TestNG_practice/dataprovider_anotherclass_practice.java
@@ -4,26 +4,11 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
-public class Dataprovider_practice {//this data are dummy it wont work
-	
-	@DataProvider(name="login",indices = {0})//this indices will pass only first row data
-	public String[][] getdata() {
-		//this data provider will run only if we have 2d array data
-		String[][] data=new String[2][2];
-		
-		data[0][0]="santhoshraj532000@gmail.com";
-		data[0][1]="Santhoshraj@5";
-		
-		data[1][0]="santhoshraj532000@gmail.com";//i dont have athother data so used same
-		data[1][1]="Santhoshraj@5";
-		
-		return data;
-	}
-	
-	@Test(dataProvider = "login")//its like dependsonmethod attribute  
+public class dataprovider_anotherclass_practice //extends Dataprovider_practice this is one way
+{
+	@Test(dataProvider = "login",dataProviderClass = Dataprovider_practice.class)
 	public void login(String email,String password) {
 		System.setProperty("webdriver.chrome.driver", "./drivers/chromedriver.exe");
 		ChromeOptions co= new ChromeOptions();
@@ -33,6 +18,5 @@ public class Dataprovider_practice {//this data are dummy it wont work
 		driver.findElement(By.name("email")).sendKeys(email,Keys.TAB,password);
 		driver.findElement(By.tagName("button")).click();
 		System.out.println(driver.getTitle());
-	}
-
+	} 
 }
